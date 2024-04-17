801b5a2c4983c6f84eadeb470dd47f7a28e2ce0e
Kapila Shobit <103351160+ShobitKapila@users.noreply.github.com>
2024-04-17 18:01:48 +0530

    Create Euclidean_algorithm_for_GCD.cpp

---
+++ Algebra/Euclidean_algorithm_for_GCD.cpp
@@ -0,0 +1,4 @@
+/* Given two non-negative integers a and b, we have to find their GCD (greatest common divisor), i.e. the largest number which is a divisor of both a and
+b. It's commonly denoted by gcd(a, b). Mathematically it is defined as:
+       gcd(a,b) = max{ k>0 : (k|a) and (k|b) }
+*/
